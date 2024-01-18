acc5d166f476ffd66b7bee317ba3d4ce99efa7fb
Alemba-Vivian <alembavivian@gmail.com>
2024-01-18 16:19:11 +0300

    recursion quizes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './Styles/style.css';
 
 import {fibs} from './quizes/fibonacci';
+import { fibsRec } from './quizes/fibonacci';
 
 
 
-fibs();
+console.log(fibs(8));  //return    [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsRec(8));  //return  [0, 1, 1, 2, 3, 5, 8, 13]
+
 
 
--- src/quizes/fibonacci.js
+++ src/quizes/fibonacci.js
@@ -10,15 +10,43 @@
 // Using an example input of 8, this function 
 //should return the array [0, 1, 1, 2, 3, 5, 8, 13].
 
+//Fibonacci sequence is a sequence in which each number is the sum of two preceding ones
+
 
 const fibs=(num)=>{
 
-    alert("I am Vivian Alemba");  //return array
+    let firstAndSecond = [0,1];
+
+    for(let i=2; i <= num; i++){
+        firstAndSecond.push(firstAndSecond[i-2] + firstAndSecond[i-1]);
+    }
+    return firstAndSecond;
+
+}
+
+
+//QUESTION 2
+//write another function fibsRec which solves the same problem recursively.
+
+
+
+const fibsRec=(n)=>{
+    if (n < 2) {
+        return [1];   
+    }
+    if (n < 3) {
+        return [1, 1];
+    }
+
+    var a = fibsRec(n - 1);
+    a.push(a[n - 2] + a[n - 3]);
+    return a;
+    
 
 
 }
 
-export{fibs}
+export{fibs, fibsRec}
 
 
 
