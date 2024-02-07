b0182265421c1d414dcb1a976b8a4aac98e95dc4
Alemba-Vivian <alembavivian@gmail.com>
2024-02-07 13:58:12 +0300

    data structures

---
+++ src/quizes/binarySearch.js
@@ -0,0 +1,20 @@
+//binary search
+//pseducode
+//1. calculate the midpoint of the current array
+//2. if the target is in the middle, stop
+//3. Otherwise, if the target is less that what's in the middle, repeat, changing the endpoint to be 
+     //to be just to left of the middle
+ //4. Otherwise, if the target is greater than what's in the middle, repeat, changing the start point 
+    //to be just to the right of the middle
+    
+    
+ // TARGET START END MIDDLE
+ 
+ //nN/B YOUR ARRRAY NEED TO BE SORTED BEFORE USING BINARY SEARCH
+
+
+ //In breadth first - we visit the nodes by level
+ //In depth first - we visit the children of the nodes
+ //<root><left><right> - Preorder
+ //<left><root><right> -Inorder
+ //<left><right><root> - Postorder
